6133a77ebbb5302f790e31f80b256066b5d5a2ee
Paulo Souza <pauloogsouza@gmail.com>
2021-01-26 00:48:01 -0300

    Fourth version using lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,11 +16,8 @@ public class Program {
                 new Product("Notebook", 1200.00),
                 new Product("Tablet", 450.00));
 
-        Comparator<Product> comp = new Comparator<>() {
-            @Override
-            public int compare(Product p1, Product p2) {
-                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-            }
+        Comparator<Product> comp = (p1, p2) -> {
+            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
         };
 
         list.sort(comp);
