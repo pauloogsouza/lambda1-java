96471e5ea3a0111051b00d15e8283f3ddae5904f
Paulo Souza <pauloogsouza@gmail.com>
2021-01-26 00:35:37 -0300

    Initial implementation

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import model.entities.Product;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = Arrays.asList(
+                new Product("TV", 900.00),
+                new Product("Notebook", 1200.00),
+                new Product("Tablet", 450.00));
+
+
+        Collections.sort(list);
+
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
